4be2e4114268646542520744eaf68a6e0b188ac8
Alex Antsiferov <wetterkrank@gmail.com>
2020-11-10 16:22:52 +0100

    Ugly but it works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,50 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :delete]
+
+  # GET /tasks
+  def index
+    @tasks = Task.all
+  end
+
+  # GET /tasks/1
+  def show
+  end
+
+  # GET /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_url
+  end
+
+  # PATCH/PUT /tasks/1
+  def update
+    @task.update(task_params)
+    redirect_to @task
+  end
+
+  # DELETE /tasks/1
+  def delete
+    @task.destroy
+    redirect_to tasks_url
+  end
+
+  
+  private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :details, :completed)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
-  get 'tasks/:id', to: 'tasks#show'
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   put 'tasks/:id', to: 'tasks#update'
+  patch 'tasks/:id', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#delete'
 end
